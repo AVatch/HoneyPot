e79f1eb0d0d57dfc01d6572b7aa6be28f38661cd
AVatch <akv224@nyu.edu>
2014-10-20 15:02:43 -0400

    some docs

--- honeyGenerator.py
+++ honeyGenerator.py
@@ -1,9 +1,24 @@
 import sys
 
 
+'''
+Main Functions
+'''
+
+
 def pollinateMe(p, k):
     pass
 
+
+def unPollinateMe(p, k):
+    pass
+
+'''
+Helper Functions
+'''
+
+
+# Execute Code
 if __name__ == '__main__':
     args = sys.argv
     pollinateMe()
